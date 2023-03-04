f76c7dce40eaba5f58825d8f23dc9576cef4cc6f
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-04 17:55:30 +0545

    form update

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdownForm");
+const dateEl = document.getElementById("date-picker");
+
+let countdownTitle = '';
+let countdownDate = '';
+// set date inpit min with today's date
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
+
+//Take Values form form input
+function updateCountdown(e){
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value
+    console.log(countdownTitle, countdownDate);
+}
+
+
+//Event Listeners
+countdownForm.addEventListener('submit', updateCountdown)
